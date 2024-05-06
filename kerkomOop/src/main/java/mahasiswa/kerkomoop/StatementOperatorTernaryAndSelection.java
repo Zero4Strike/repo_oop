d76171daf75cf39b92c20f96630e6fbc3eabe45e
Lenovo <Zero4Strike>
2024-05-06 13:27:07 +0700

    Add new file java StatementOperatorTernaryAndSelection

---
+++ kerkomOop/src/main/java/mahasiswa/kerkomoop/StatementOperatorTernaryAndSelection.java
@@ -0,0 +1,22 @@
+package mahasiswa.kerkomoop;
+
+import java.util.Scanner;
+public class StatementOperatorTernaryAndSelection {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukkan usia: ");
+        int usia = input.nextInt();
+
+        System.out.print("Apakah Anda memiliki SIM? (ya/tidak): ");
+        String sim = input.next();
+
+        if (usia >= 18 && sim.equalsIgnoreCase("ya")) {
+        System.out.println("Anda dapat mengemudikan mobil.");
+        } else if (usia >= 16 && sim.equalsIgnoreCase("ya")) {
+        System.out.println("Anda dapat mengemudikan sepeda motor.");
+        } else {
+        System.out.println("Anda tidak dapat mengemudikan kendaraan.");
+        }
+    }
+}
